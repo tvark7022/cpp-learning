e514219978479244a335caf6214e461688adb570
Dmitry <tvark7022@users.noreply.github.com>
2020-12-28 17:35:33 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//comment 
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    
+    cout << "Hello, world!\n";
+    system ("pause");
+    cin:get();
+    return 0;
+}
